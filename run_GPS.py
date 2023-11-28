6a4f87652a0085a6e0f01cd156cb9112b9dcdd42
Inna <inna.vik19@gmail.com>
2023-11-28 21:04:00 +0300

    create run files

---
+++ run_GPS.py
@@ -0,0 +1,11 @@
+import os
+
+
+def main():
+    print('start script')
+    os.system('"python example.py -t -x GPS"')
+    print('end script')
+
+
+if __name__ == '__main__':
+    main()
---
+++ run_UAA.py
@@ -0,0 +1,11 @@
+import os
+
+
+def main():
+    print('start script')
+    os.system('"python example.py -t -x UAA"')
+    print('end script')
+
+
+if __name__ == '__main__':
+    main()
